2e8358148b9527f34aa52166d94428d18be51a88
garonbailey <bailegm@gmail.com>
2015-10-09 11:48:25 -0400

    Updated server to use my dependencies

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-var express = require('express'),
-	PORT    = process.env.PORT || 5432,
-	server  = express(),
-	MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
-	dbname   = /* set here as string */,
-	mongoose = require('mongoose');
+var express        = require('express'),
+	PORT           = process.env.PORT || 5432,
+	server         = express(),
+	MONGOURI       = /* process.env.MONGOLAB_URI || */ "mongodb://localhost:27017",
+	dbname         = /* set here as string */,
+	mongoose       = require('mongoose'),
+	Schema         = mongoose.Schema,
+	ejs            = require('ejs'),
+	session        = require('express-session'),
+	bodyParser     = require('body-parser'),
+	methodOverride = require('method-override');
+
+server.set('views', './views');
+server.set('view engine', 'ejs');
+
+server.use(express.static('./public'));
+server.use(session({
+	secret: "mischievousCat",
+	resave: false,
+	saveUninitialized: false
+}));
+server.use(bodyParser.urlencoded({
+	extended: true
+}));
+server.use(methodOverride('_method'));
 
 
 server.get('/test', function (req, res) {
